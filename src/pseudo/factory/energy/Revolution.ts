3c977da0a0232a4846ed239c359549868fe49c4a
Will Hay <willhayjr@gmail.com>
2022-11-10 12:49:57 -0800

    entropy and creator

--- src/pseudo/factory/energy/Revolution.ts
+++ src/pseudo/factory/energy/Revolution.ts
@@ -31,13 +31,34 @@ class Revolution {
 
   /*
 
-    The proportion of the auction proceeds that go to the proposer
+    The min. proportion of the auction proceeds that go to the creator
     A number between 0 and 1 (inclusive)
-    For example, a split rate of 0.7 indicates that 70% of an auction's
-    proceeds will go to the proposer (in the form of governance stake OR cash)
+    For example, a minimum creator rate of 0.2 indicates that at least 20% 
+    of an auction's proceeds will go to the creator.
+    (in the form of governance stake OR cash)
+    //Higher creator rates incentivize creators to create more valuable auctions
+    //Lower creator rates gives more power to capital
+    //This is a very important parameter
 
   */
-  public splitRate?: number;
+  public minCreatorRate?: number;
+
+  /*
+  
+    The proportion of the creator's share of auction proceeds 
+    that go directly to the creator in the form of hard cash.
+    A number between 0 and 1 (inclusive).
+    eg: a creator cash rate of 0.2 indicates that 20% of the creator's share
+    of auction proceeds will go directly to the creator in the form of hard cash.
+    The remaining 80% will go to the creator in the form of governance stake.
+    //Higher creator cash rates incentivize creators who want to be paid in cash
+    //Lower creator cash rates incentivize creators who want to be paid in governance stake
+    //This is a very important parameter
+
+    //TODO investigate fixed rate vs. percentage rate
+
+  */
+  public defaultEntropyRate?: number;
 
   public auctionPeriods: AuctionPeriod[];
   public submissionPeriods: SubmissionPeriod[];
@@ -55,6 +76,8 @@ class Revolution {
     submissionPeriodConfig: SubmissionPeriodConfig,
     votingPeriodConfig: VotingPeriodConfig,
     auctionPeriodConfig: AuctionPeriodConfig,
+    minimumCreatorRate: number,
+    defaultEntropyRate: number,
     mission: string = "Make the world a better place"
   ) {
     this.auctionPeriods = [];
@@ -66,6 +89,9 @@ class Revolution {
     this.auctionPeriodConfig = auctionPeriodConfig;
 
     this.mission = mission;
+
+    this.minCreatorRate = minimumCreatorRate;
+    this.defaultEntropyRate = defaultEntropyRate;
   }
 
   //initiate revolution
@@ -200,8 +226,21 @@ class Revolution {
   //decided upon by a collective measure
   //many many dynamics and theories around this
   //for now, just a simple function
-  protected updateSplitRate(splitRate: number) {
-    this.splitRate = splitRate;
+  //a high min creator rate incentivizes creators with
+  //either cash or governance stake
+  //ONLY THE COLLECTIVE CAN VOTE TO CHANGE THIS
+  protected updateCreatorRate(rate: number) {
+    this.minCreatorRate = rate;
+  }
+
+  //decided upon by a collective measure
+  //many many dynamics and theories around this
+  //for now, just a simple function
+  //a higher entropy rate incentivizes creators to create more complex
+  //and diverse submissions by paying them cash for doing so
+  //ONLY THE COLLECTIVE CAN VOTE TO CHANGE THIS
+  protected updateDefaultEntropyRate(rate: number) {
+    this.defaultEntropyRate = rate;
   }
 
   //decided upon by a collective measure
